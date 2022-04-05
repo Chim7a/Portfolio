ebce4149ea181eca093721bd80a94684982c99d4
Chim7a <80280391+Chim7a@users.noreply.github.com>
2022-04-05 19:25:27 +0100

    Fonts applied.

--- src/components/View/About/About.js
+++ src/components/View/About/About.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import Tilt from 'react-parallax-tilt';
+
 
 
 const About = () => {
   return (
-    <div className="sm:flex sm:flex-col lg:flex lg:flex-col lg:justify-center bg-rose-600 text-white pt-5 pb-5 " id="about">
-            <div className="text-center">
+    <div className="flex flex-col justify-center bg-rose-600 text-white " id="about">
+            <div className="text-center mt-5">
             <span className="text-3xl font-bold">ABOUT ME</span>
             </div>
-        <div className="relative lg:ml-20 sm:flex sm:flex-col sm:items-center m-10 md:grid md:grid-cols-2 md:justify-items-center lg:flex lg:flex-row gap-5">
-        <img src="chima.jpg" width="350px" height="auto" alt="Chima'simage" />
-        
-
-        <div className="text-2xl sm:text-start md:text-start ">
-        <p> Lorem ipsum dolor sit ameugfuyuyt, consectetur adipiscing elit. Sed id magna ac urna lobortis egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam ac risus massa. Duis sodales lobortis ex sed rutrum. Pellentesque bibendum justo fringilla, ultrices nisl nec, accumsan massa. In ultrices vel libero a efficitur. Etiam tempor, justo non viverra iaculis, metus enim commodo turpis, nec efficitur mi nibh placerat elit. Quisque ullamcorper, felis vel dapibus posuere, lacus tellus vestibulum lorem, quis lacinia lorem tortor eu tortor. In congue finibus nunc, ut auctor ex. Sed eget ipsum ut ex rhoncus ornare. Maecenas fringilla neque lorem, eget euismod massa ultricies vel. Integer non augue mauris. Nulla facilisi. Mauris ullamcorper venenatis diam, tristique semper erat ornare et. Pellentesque tempor nec est non cursus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed auctor scelerisque orci, dictum ornare nisi. Proin vitae vulputate eros. Morbi blandit dapibus nunc, ut eleifend diam tempor nec. Sed scelerisque diam non sapien egestas dictum. Donec tempus vestibulum orci ut fringilla. Vivamus nec enim justo. Cras semper nisl metus, nec volutpat leo lacinia nec. </p>
-        </div>
+        <div className="text-2xl m-5">
+        <p> A highly motivated, self-driven and passionate frontend web developer. Strong knowledge in creating 
+          solutions to complex problems while developing innovative projects with simplicity. 
+          Experienced in creating fast and responsive websites with tasks executed before deadline, 
+          while maintaining codebase standards and practices. 
+          Now seeking a new opportunity to further my career in a Junior development role. </p>
         </div>
     </div>
   )
